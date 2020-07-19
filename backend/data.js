de105d4640bf80165c80da3fef720a52c87529cb
Thien Tran <thien.tran@hnv-tech.com>
2020-07-19 16:26:51 +0200

    update frontend and backend

---
+++ backend/data.js
@@ -0,0 +1,59 @@
+export default {
+    products:[{
+        _id : "1",
+        name : 'Slime Shirt',
+        image: '../images/d1.jpg',
+        price : 60,
+        brand : 'Nike',
+        rating : 4.5,
+        numReviews : 10,
+        countInStock : 0
+    },{
+        _id : "2",
+        name : 'L Shirt',
+        image: '../images/d1.jpg',
+        price : 50,
+        brand : 'Lacoste',
+        rating : 4.5,
+        numReviews : 10,
+        countInStock : 5
+    },
+    {
+        _id : "3",
+        name : 'Slime Shirt',
+        image: '../images/d1.jpg',
+        price : 60,
+        brand : 'Nike',
+        rating : 4.5,
+        numReviews : 10,
+        countInStock : 5
+    },
+    {
+        _id : "4",
+        name : 'Slime Shirt',
+        image: '../images/d1.jpg',
+        price : 60,
+        brand : 'Nike',
+        rating : 4.5,
+        numReviews : 10,
+        countInStock : 5
+    },
+    {
+        _id : "5",
+        name : 'Slime Shirt',
+        image: '../images/d1.jpg',
+        price : 60,
+        brand : 'Nike',
+        rating : 4.5,
+        numReviews : 10
+    },
+    {
+        _id : "6",
+        name : 'Slime Shirt',
+        image: '../images/d1.jpg',
+        price : 60,
+        brand : 'Nike',
+        rating : 4.5,
+        numReviews : 10
+    }]
+}
---
+++ backend/server.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import data from './data';
+import config from './config';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import userRoute from './routes/userRoute';
+dotenv.config();
+const mongodbUrl = config.MONGODB_URL;
+
+mongoose.connect(mongodbUrl,{
+    useNewUrlParser:true,
+    useUnifiedTopology: true,
+    useCreateIndex :true
+}).catch(error => console.log(error.reason));
+
+const app = express();
+
+app.use("/api/users",userRoute)
+
+app.get("/api/products/:id", (req,res) =>{
+    const productId = req.params.id;
+    const product = data.products.find(x =>x._id === productId);
+    console.log(product);
+    if(product)
+        res.send(product);
+    else
+        res.status(404).send({msg : "Product Not Found."})
+})
+app.get("/api/products", (req,res) =>{
+    res.send(data.products);
+})
+
+app.listen(5000, () => { console.log("Server started at http://localhost:5000")})
