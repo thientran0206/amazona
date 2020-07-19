bd690784208b0b04a47a76694cbf40a547559ac3
Thien Tran <thien.tran@hnv-tech.com>
2020-07-19 16:34:46 +0200

    update front

---
+++ frontend/src/data.js
@@ -0,0 +1,55 @@
+export default {
+    products:[{
+        _id : "1",
+        name : 'Slime Shirt',
+        image: '../images/d1.jpg',
+        price : 60,
+        brand : 'Nike',
+        rating : 4.5,
+        numReviews : 10
+    },{
+        _id : "2",
+        name : 'L Shirt',
+        image: '../images/d1.jpg',
+        price : 50,
+        brand : 'Lacoste',
+        rating : 4.5,
+        numReviews : 10
+    },
+    {
+        _id : "3",
+        name : 'Slime Shirt',
+        image: '../images/d1.jpg',
+        price : 60,
+        brand : 'Nike',
+        rating : 4.5,
+        numReviews : 10
+    },
+    {
+        _id : "4",
+        name : 'Slime Shirt',
+        image: '../images/d1.jpg',
+        price : 60,
+        brand : 'Nike',
+        rating : 4.5,
+        numReviews : 10
+    },
+    {
+        _id : "5",
+        name : 'Slime Shirt',
+        image: '../images/d1.jpg',
+        price : 60,
+        brand : 'Nike',
+        rating : 4.5,
+        numReviews : 10
+    },
+    {
+        _id : "6",
+        name : 'Slime Shirt',
+        image: '../images/d1.jpg',
+        price : 60,
+        brand : 'Nike',
+        rating : 4.5,
+        numReviews : 10
+    }]
+}
---
+++ frontend/src/store.js
@@ -0,0 +1,17 @@
+import {createStore,combineReducers, compose, applyMiddleware} from 'redux';
+import { productListReducer, productDetailsReducer } from './reducers/productReducers';
+import { cartReducer} from './reducers/cartReducer';
+import thunk from 'redux-thunk';
+import Cookie from 'js-cookie';
+
+const cartItems = Cookie.getJSON("cartItems") || [];
+const initialState = {cart: {cartItems}};
+console.log( Cookie.getJSON("cartItems"));
+const reducer = combineReducers({
+    productList : productListReducer,
+    productDetails : productDetailsReducer,
+    cart : cartReducer
+})
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer,initialState,composeEnhancer(applyMiddleware(thunk)));
+export default store;
